c7bf9f74ce0749c25b6b9fe0ee8600ffdd97f1c4
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-07-07 12:05:38 +0200

    commit the changes

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            string playAgain = "yes";
+
+            while (playAgain.ToLower() == "yes")
+            {
+                // Generate a random magic number between 1 and 100
+                Random randomGenerator = new Random();
+                int magicNumber = randomGenerator.Next(1, 101);
+
+                int guess = 0;
+                int guessCount = 0;
+
+                Console.WriteLine("I have chosen a magic number between 1 and 100.");
+
+                // Keep asking until the user guesses correctly
+                while (guess != magicNumber)
+                {
+                    Console.Write("What is your guess? ");
+                    guess = int.Parse(Console.ReadLine());
+
+                    guessCount++;
+
+                    if (guess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+                }
+
+                Console.WriteLine($"It took you {guessCount} guesses.");
+
+                Console.Write("Do you want to play again? (yes/no): ");
+                playAgain = Console.ReadLine();
+            }
+
+            Console.WriteLine("Thanks for playing!");
+        }
     }
+}
 }
