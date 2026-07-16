e33129f8c5bfadb18b7fa8428b66fe914376cea1
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-07-16 18:56:59 +0200

    Add Fraction.cs file

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,79 @@
+using System;
+
+public class Fraction
+{
+    // Private attributes
+    private int _top;
+    private. int _bottom;
+
+    // Constructor: 1/1
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    //Constructor: top/bottom
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Getters
+
+    public int GetTop()
+
+    {
+
+        return _top;
+
+    }
+
+    public int GetBottom()
+
+    {
+
+        return _bottom;
+
+    }
+
+    // Setters
+
+    public void SetTop(int top)
+
+    {
+
+        _top = top;
+
+    }
+
+    public void SetBottom(int bottom)
+
+    {
+
+        _bottom = bottom;
+
+    }
+
+    // Return fraction as a string
+
+    public string GetFractionString()
+
+    {
+
+        return $"{_top}/{_bottom}";
+
+    }
+
+    // Return decimal value
+
+    public double GetDecimalValue()
+
+    {
+
+        return (double)_top / _bottom;
+
+    }
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -3,7 +3,77 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+
+        Fraction fraction2 = new Fraction(6);
+
+        Fraction fraction3 = new Fraction(6, 7);
+
+        Console.WriteLine("Fractions created with constructors:");
+
+        Console.WriteLine(fraction1.GetFractionString());
+
+        Console.WriteLine(fraction2.GetFractionString());
+
+        Console.WriteLine(fraction3.GetFractionString());
+
+        Console.WriteLine();
+
+        // Test getters
+
+        Console.WriteLine("Top: " + fraction3.GetTop());
+
+        Console.WriteLine("Bottom: " + fraction3.GetBottom());
+
+        Console.WriteLine();
+
+        // Test setters
+
+        fraction3.SetTop(3);
+
+        fraction3.SetBottom(4);
+
+        Console.WriteLine("After using setters:");
+
+        Console.WriteLine(fraction3.GetFractionString());
+
+        Console.WriteLine("Decimal: " + fraction3.GetDecimalValue());
+
+        Console.WriteLine();
+
+
+        Fraction one = new Fraction();
+
+        Fraction five = new Fraction(5);
+
+        Fraction threeFourths = new Fraction(3, 4);
+
+        Fraction oneThird = new Fraction(1, 3);
+
+        Console.WriteLine(one.GetFractionString());
+
+        Console.WriteLine(one.GetDecimalValue());
+
+        Console.WriteLine();
+
+        Console.WriteLine(five.GetFractionString());
+
+        Console.WriteLine(five.GetDecimalValue());
+
+        Console.WriteLine();
+
+        Console.WriteLine(threeFourths.GetFractionString());
+
+        Console.WriteLine(threeFourths.GetDecimalValue());
+
+        Console.WriteLine();
+
+        Console.WriteLine(oneThird.GetFractionString());
+
+        Console.WriteLine(oneThird.GetDecimalValue());
+
     }
+
 }
